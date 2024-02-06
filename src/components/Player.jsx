9532b13da815a0474b7b85398eb64757dfbdb645
Asa2468 <156419274+Asa2468@users.noreply.github.com>
2024-02-06 11:27:28 +0530

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -44,13 +44,10 @@ export default function Player({
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
-  // let btnCaption = 'Edit';
-
   if (isEditing) {
     editablePlayerName = (
       <input type="text" required value={playerName} onChange={handleChange} />
     );
-    // btnCaption = 'Save';
   }
 
   return (
